29d8a4db29bd67b20a1e1ac9e54e8cfb28de2117
Tatiana <Tatiana@bk.ru>
2022-08-08 12:09:05 +0400

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+string [] array = {"hello", "2", "world", ":-)"};
+Print (array);
+newArray(array);
+
+void newArray (string [] array)
+{
+     string[] newarr = new string[array.Length];
+     for (int i=0; i<array.Length; i++)
+     {      
+        if(array[i].Length<=3)
+        {
+        newarr[i]=array[i];;
+        Write($"{newarr[i]} "); 
+        }
+     }
+        WriteLine(); 
+}
+void Print(string [] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        Write($"{array[i]} ");
+    }
+    WriteLine();
+}
